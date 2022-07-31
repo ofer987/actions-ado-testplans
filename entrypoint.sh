d348b8cdfb6625a43baffc1f713624ac815130d4
PavanMudigondaTR <nagapavankumar.mudigonda@thomsonreuters.com>
2022-07-31 16:25:31 -0400

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #! /usr/bin/env sh
 
-python /action/src/runner.py run test all
+cd /action
+# Set ADO ORG URL
+python src/runner.py config url --set-to ${INPUT_ADO_ORG_URL}
+# Set ADO PAT
+python src/runner.py config pat --set-to ${INPUT_ADO_PAT}
+
+# RUN ADO TEST MODULE
+python /src/runner.py run test all
