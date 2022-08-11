d3fbef0cb57a7286243715222586d352fa84af0a
Pavan Mudigonda <86745613+PavanMudigondaTR@users.noreply.github.com>
2022-08-11 09:18:15 -0400

    Delete test.sh

--- test.sh
+++
@@ -1,3 +0,0 @@
-ADO_RUN_NUM_STRING="$( grep 'results as Test Run' output.txt | awk -F'[()]' '{print $2}')";
-ADO_RUN_NUM="$(echo $ADO_RUN_NUM_STRING | tr -dc '0-9' | sed 's/%//g')";
-echo $ADO_RUN_NUM;
