5b769aab54cee4c9852244cccd22417af7e4ca44
PavanMudigondaTR <PavanMudigondaTR@users.noreply.github.com>
2023-01-28 19:19:11 +0000

    Updated CS with Azure DevOps Tags
    Signed-off-by: GitHub <noreply@github.com>

--- nunit/NUnitSample/NUnitSample/UnitTest1.cs
+++ nunit/NUnitSample/NUnitSample/UnitTest1.cs
@@ -9,12 +9,14 @@ namespace NUnitTestProject
         [Test]
         [Category("MyCategory")]
         [Category("story:17372")]
+        [Category("tc:24071")]
         public void OnePassingTest()
         {
             // perform test
         }
 
         [Test]
+        [Category("tc:24072")]
         public void OneFailingTest()
         {
             Console.WriteLine("This is a passing test");
@@ -25,6 +27,7 @@ namespace NUnitTestProject
         [TestCase("foo", 1, Description = "First")]
         [TestCase("bar", 2, Description = "Second")]
         [TestCase("baz", 3, Description = "Third")]
+        [Category("tc:24073")]
         public void SampleDataDriven(string a, int b)
         {
             Console.WriteLine($"Testing: {a}, {b}");
