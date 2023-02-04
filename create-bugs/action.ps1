d6c5e055afe780843a6044270b6138549eb4575c
PavanMudigondaTR <nagapavankumar.mudigonda@thomsonreuters.com>
2023-02-03 19:04:28 -0500

    updated

--- create-bugs/action.ps1
+++ create-bugs/action.ps1
@@ -220,15 +220,16 @@ $projects.value  | ForEach-Object {
 "@
                     
                     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-                    $getLinkedBugs = Invoke-RestMethod $getLinkedBugURI -Method Get -ContentType "application/json" -Headers $header
                     $bugWorkItemURI = Invoke-RestMethod $createBugWorkItemUrl -Method POST -ContentType "application/json-patch+json" -Headers $header -Body $body
                     Write-Host "Bug created for failed test case" $bugWorkItemURI.id -ForegroundColor Blue
                     $bugID = $bugWorkItemURI.id
+                    "Failed Test ID: [$testCaseID](https://dev.azure.com/$organization/$project/_testManagement/runs?runId=$lastRunId&_a=resultSummary&resultId=$resultID) Linked with New Bug Id: [$bugID](https://dev.azure.com/$organization/$project/_workitems/edit/$bugID)" >> $env:GITHUB_STEP_SUMMARY
                     $parentRelation = 'System.LinkTypes.Hierarchy-Forward'
+                    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+                    $getLinkedBugs = Invoke-RestMethod $getLinkedBugURI -Method Get -ContentType "application/json" -Headers $header
                     $findExistingBugs = $getLinkedBugs | where-object { $_.rel -match $parentRelation -and $_.attributes.comment -match "Created by ADO Test Automation"}
                     $existingBugId = $findExistingBugs.id
                     Write-Host "Existing bug: $existingBugId"
-                    "Failed Test ID: [$testCaseID](https://dev.azure.com/$organization/$project/_testManagement/runs?runId=$lastRunId&_a=resultSummary&resultId=$resultID) Linked with New Bug Id: [$bugID](https://dev.azure.com/$organization/$project/_workitems/edit/$bugID)" >> $env:GITHUB_STEP_SUMMARY
                 }
                 else {
                     Write-Host "All Tests Passed"
