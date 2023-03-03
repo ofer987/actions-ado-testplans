60e9e4bfd0892ee1bf7f1d65b631ad80c6e56035
PavanMudigondaTR <nagapavankumar.mudigonda@thomsonreuters.com>
2023-03-03 12:25:08 -0500

    updted

--- cucumber/test-results.sh
+++ cucumber/test-results.sh
@@ -6,43 +6,48 @@
 # Feel Free to Copy The Shell Script to your Project GitHub Repo.
 # But Please send us a note or mark Star if you are using this script so we can help you with future changes or do troubleshooting
 # Please provide your secrets very securely using AWS Secret Manager otherwise the keys will get printed in logs which will leak passwords
-env='QA'
-TEST_RESULTS_WITH_ATTACHMENTS='false'
-TEST_RESULTS_ATTACHED_FILES="NOT APPLICABLE"
+export CODEBUILD_BUILD_ID=123456789
+export ENVIRONMENT=QA
+export GH_PAGES_TEST_RUN_URL=https://thomsonreuters.com/
+env=${ENVIRONMENT}
+AWS_CB_HOME=$PWD
 ARTIFACTORY_TOKEN=${ARTIFACTORY_TOKEN}
 ARTIFACTORY_USER=${ARTIFACTORY_USER}
 ADO_PAT=${ADO_PAT}
+AREA_PATH="\\OSPI Release Automation QA"
+ITERATION_PATH="\\Test"
+TEST_PLAN_ID=832988
 RUN_COMMENT=${GH_PAGES_TEST_RUN_URL}
 RUN_RESULT_COMMENT=${GH_PAGES_TEST_RUN_URL}
-ADO_PROJECT_URL=https://dev.azure.com/tr-ihn-sandbox/
-CUCUMBER_PATH="./features"
+ADO_PROJECT_URL=https://dev.azure.com/tr-corp-tax/CTTSharedServices
+CUCUMBER_PATH="src/test/features"
 TEST_RESULTS_RUN_NAME="AWS CodeBuild - ${CODEBUILD_BUILD_ID}"
-TEST_RESULTS_PATH="results/cucumber.json"
+TEST_RESULTS_PATH="${AWS_CB_HOME}/.github/cucumber_report.json"
 TEST_RESULTS_FORMAT="cucumberJson"
+#download specsync json
+curl -u ${ARTIFACTORY_USER}:${ARTIFACTORY_TOKEN} -O 'https://tr1.jfrog.io/artifactory/generic-local/devops-experience/cucumber/specsync.json'
+CWD=$PWD
+SPECSYNC_JSON_PATH=$CWD/specsync.json
+echo "SpecSync Json Path: $SPECSYNC_JSON_PATH"
 # Make sure you have such config present in ADO test plans what ever entered here
-TEST_RESULTS_CONFIG="Linux-X64"
-CHILD_JSON=$(find . -iname "specsync.json" | tr '\n' ',' )
-PARENT_JSON=$(find . -iname "specsync-parent.json" )
-echo CHILD_JSON=${CHILD_JSON}
-echo PARENT_JSON=${PARENT_JSON}
-echo "Child Json: $CHILD_JSON"
-echo "Parent Json: $PARENT_JSON"
+TEST_RESULTS_CONFIG="Chrome"
+
 
 # VARIABLE SUBSTITUTION IN SPECSYNC JSON DYNAMICALLY 
 
-sed -i "s/##ADO_PROJECT_URL##/$ADO_PROJECT_URL/g" **/specsync.json
-sed -i "s/##ADO_PAT##/$ADO_PAT/g" **/specsync.json
-sed -i "s/##TEST_RESULTS_RUN_NAME##/$TEST_RESULTS_RUN_NAME/g" **/specsync.json
-
-sed -i "s/##ADO_PROJECT_URL##/$ADO_PROJECT_URL/g" **/specsync-parent.json
-sed -i "s/##ADO_PAT##/$ADO_PAT/g" **/specsync-parent.json
-sed -i "s/##TEST_RESULTS_RUN_NAME##/$TEST_RESULTS_RUN_NAME/g" **/specsync-parent.json
-
+sed -i -e "s|ADO_PROJECT_URL|$ADO_PROJECT_URL|g" $SPECSYNC_JSON_PATH
+sed -i -e "s|ADO_PAT|$ADO_PAT|g" $SPECSYNC_JSON_PATH
+sed -i -e "s|ADO_AREA_PATH|$AREA_PATH|g" $SPECSYNC_JSON_PATH
+sed -i -e "s|ADO_ITERATION_PATH|$ITERATION_PATH|g" $SPECSYNC_JSON_PATH
+sed -i -e "s|999999|$TEST_PLAN_ID|g" $SPECSYNC_JSON_PATH
+sed -i -e "s|ADO_DUMMY_URL|$ADO_PROJECT_URL|g" $SPECSYNC_JSON_PATH
 
 # Download SpecSync License From Artifactory
 curl -u ${ARTIFACTORY_USER}:${ARTIFACTORY_TOKEN} -O 'https://tr1.jfrog.io/artifactory/generic-local/devops-experience/specsync.lic'
-CUCUMBER_DIRNAME=$(dirname ${CUCUMBER_PATH} )
-mv specsync.lic ${CUCUMBER_DIRNAME}
+CWD=$PWD
+LICENSE_PATH=$CWD/specsync.lic
+echo LICENSE_PATH=${LICENSE_PATH}
+echo "SpecSync License Path: $LICENSE_PATH"
 
 # DOTNET INSTALL 6.0         
 # https://dot.net/v1/dotnet-install.sh
@@ -57,7 +62,7 @@ dotnet new tool-manifest
 dotnet tool install SpecSync.AzureDevOps --version 3.4.0
 dotnet specsync version
 
-results_with_attachments() {
+test_results_sync() {
     find ${CUCUMBER_PATH} -type f -name "*.feature" | while read line;
     do
         CWD=$PWD
@@ -65,46 +70,40 @@ results_with_attachments() {
         DIR=$(dirname "$line")
         echo $DIR
         cd $DIR
-        FEATURE_NAME=$(basename $DIR)
-        dotnet specsync publish-test-results --user "${ADO_PAT}" -v --log output.txt --zeroExitCodeForWarnings -r "${TEST_RESULTS_PATH}" -f "${TEST_RESULTS_FORMAT}" -c "${TEST_RESULTS_CONFIG}" --runName "${TEST_RESULTS_RUN_NAME}-Env:${ENV}-Suite:${FEATURE_NAME}" --runComment "${{ inputs.RUN_COMMENT }}" --runResultComment "${{ inputs.RUN_RESULT_COMMENT }}" 2> /dev/null;
+        DIR="${DIR%/}"
+        SUBDIR="${DIR##*/}"
+        FILE=specsync.json
+        if [ -f "$FILE" ]; then
+        echo "$FILE exists."
+        else 
+            echo "$FILE does not exist."
+            cp ${SPECSYNC_JSON_PATH} ./
+            tmp=$(mktemp)
+            jq --arg a "$SUBDIR" '.remote.testSuite.name = $a' ./specsync.json > "$tmp" && mv "$tmp" ./specsync.json
+        fi
+        FEATURE_NAME=$(basename $DIR)      
+        dotnet specsync publish-test-results --license ${LICENSE_PATH} --user ${ADO_PAT} -v --log output.txt --zeroExitCodeForWarnings -r "${TEST_RESULTS_PATH}" -f "${TEST_RESULTS_FORMAT}" -c "${TEST_RESULTS_CONFIG}" --runName "${TEST_RESULTS_RUN_NAME}-Env:${ENVIRONMENT}-Suite:${FEATURE_NAME}" --runComment "${RUN_COMMENT}" --testResultComment "${RUN_RESULT_COMMENT}" 2> /dev/null;
         cat output.txt;
         ADO_SUITE_RUN_URL=$(grep 'results as Test Run' output.txt | sed 's/.*[(] *//'| sed 's/).//g')
-        ADO_SUITE_RUN_URL="$ADO_SUITE_RUN_URL"
-        echo "ADO_SUITE_RUN_URL=$ADO_SUITE_RUN_URL" >> $GITHUB_ENV
-        echo "ADO_SUITE_RUN_URL=$ADO_SUITE_RUN_URL" >> $GITHUB_OUTPUT
-        echo "TEST SUITE RUN URL for ${DIR} : ${ADO_SUITE_RUN_URL}"
+        echo "ADO_SUITE_RUN_URL=$ADO_SUITE_RUN_URL"
+        echo "ADO_SUITE_RUN_URL=$ADO_SUITE_RUN_URL"
+        echo "TestSuite Run [${DIR}](${ADO_SUITE_RUN_URL})"
+        ADO_RUN_ID=$(echo $ADO_SUITE_RUN_URL | awk -F'=' '{print $2;}' | sed 's|&_a||g')
+        ADO_RUN_ID=()
+        ADO_RUN_ID+=("$ADO_RUN_ID")
+        ADO_RUN_URL=()
+        ADO_RUN_URL+=("$ADO_SUITE_RUN_URL")
         cd $CWD
     done;
+    echo "ADO_PROJECT_URL=${ADO_PROJECT_URL}/_testManagement/runs?_a=runQuery"
+    ADO_RUN_ID_ARRAY=$(echo "${ADO_RUN_ID[@]}")
+    ADO_RUN_URL_ARRAY=$(echo "${ADO_RUN_URL[@]}")
+    echo "ADO_RUN_ID_ARRAY=$(echo "${ADO_RUN_ID[@]}")"
+    echo "ADO_RUN_URL_ARRAY=$(echo "${ADO_RUN_URL[@]}")"
 }
 
-results_without_attachments() {
-    # Run Test Results Synchronization with Attachments
-    find ${CUCUMBER_PATH} -type f -name "*.feature" | while read line;
-    do
-        CWD=$PWD
-        line=$(echo $line)
-        DIR=$(dirname "$line")
-        echo $DIR
-        cd $DIR
-        FEATURE_NAME=$(basename $DIR)
-        dotnet specsync publish-test-results --user "${ADO_PAT}" -v --log output.txt --zeroExitCodeForWarnings -r "${TEST_RESULTS_PATH}" -f "${TEST_RESULTS_FORMAT}" -c "${TEST_RESULTS_CONFIG}" --attachedFiles "${TEST_RESULTS_ATTACHED_FILES}" --runName "${TEST_RESULTS_RUN_NAME}-Env:${ENV}-Suite:${FEATURE_NAME}" --runComment "${{ inputs.RUN_COMMENT }}" --runResultComment "${{ inputs.RUN_RESULT_COMMENT }}" 2> /dev/null;
-        cat output.txt
-        ADO_SUITE_RUN_URL=$(grep 'results as Test Run' output.txt | sed 's/.*[(] *//'| sed 's/).//g')
-        ADO_SUITE_RUN_URL="$ADO_SUITE_RUN_URL"
-        echo "ADO_SUITE_RUN_URL=${ADO_SUITE_RUN_URL}" >> $GITHUB_OUTPUT
-        echo "TEST SUITE RUN URL for ${DIR} : ${ADO_SUITE_RUN_URL}"
-        cd $CWD
-    done;
-
-}
-
-# Run Test Results Synchronization with out Attachments
-if [TEST_RESULTS_WITH_ATTACHMENTS == 'true']
-then
-   results_with_attachments
-elif [TEST_RESULTS_WITH_ATTACHMENTS == 'false']
-   results_without_attachments
-fi
+# Call Function
+test_results_sync
 
 # Erase SpecSync License Files
 rm -rf ${CUCUMBER_PATH}/specsync.lic
